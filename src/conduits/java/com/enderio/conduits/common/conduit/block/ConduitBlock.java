e8a20170fbd24b92bf75aeacbe8c3aec3fb557dd
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-10-29 22:42:10 +0000

    fix: Tank recipes being erroneously mutated by the block entity
    Closes: GH-877

--- src/conduits/java/com/enderio/conduits/common/conduit/block/ConduitBlock.java
+++ src/conduits/java/com/enderio/conduits/common/conduit/block/ConduitBlock.java
@@ -290,7 +290,7 @@ public class ConduitBlock extends Block implements EntityBlock, SimpleWaterlogge
                     event.getLevel().playSound(null, event.getPos(), SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 0.2f,
                         ((player.getRandom().nextFloat() - player.getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F);
                 }
-                
+
                 event.setCanceled(true);
             }
         }
